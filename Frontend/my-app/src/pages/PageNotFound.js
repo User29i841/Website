c91b2a122503a559a94a21766f360212a312dcee
TerryDavisGhost <Genshinuwuegirl@gmail.con>
2024-09-28 19:36:07 -0400

    merging stuff

--- Frontend/my-app/src/pages/PageNotFound.js
+++ Frontend/my-app/src/pages/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound() {
   return (
     <div className='pagenotfound-container'>
       <div className="pagenotfound-content">
-        <h1>404 - Page Not Found</h1>
+        <h4>404 - Page Not Found</h4>
         <p>Oops! The page you're looking for doesn't exist.</p>
         <Link to="/">
           <button className="home-button">Take me Home</button>
