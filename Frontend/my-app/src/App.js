6b3f10cc75937edb0044027228a504cb61d0ff52
TerryDavisGhost <129913073+User29i841@users.noreply.github.com>
2024-09-28 19:25:29 -0400

    Update App.js

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,22 +1,23 @@
-
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import CoinFlip from './pages/CoinFlip'
-import NavBar from './components/Navbar'
-import SideBar from './components/SideBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CoinFlip from './pages/CoinFlip';
+import NavBar from './components/Navbar';
+import SideBar from './components/SideBar';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <NavBar />
-      <SideBar>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/coinflip' element={<CoinFlip/>}/>
-        </Routes>
-      </SideBar>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <SideBar>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/coinflip' element={<CoinFlip />} />
+            <Route path='*' element={<PageNotFound />} /> 
+          </Routes>
+        </SideBar>
+      </BrowserRouter>
     </>
   );
 }
